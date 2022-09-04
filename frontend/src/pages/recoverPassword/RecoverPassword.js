4122a98c74997fcf0e17a0e6ab31066dfc95531b
io <gabi>
2022-09-04 11:31:11 +0200

    next

--- frontend/src/pages/recoverPassword/RecoverPassword.js
+++ frontend/src/pages/recoverPassword/RecoverPassword.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const RecoverPassword = () => {
+  const [email,setEmail]=useState("")
+  const [key,setKey]=useState("")
+
+
+  const submitHandler= async(e)=>{
+    e.preventDefault()
+
+    const {data}= await axios.post("/api/users/forgot-password",{
+    email
+    }
+    )
+    setKey(data)
+  }
+  console.log(key);
   return (
-    <div>RecoverPassword</div>
+    <div>
+      <form onSubmit={submitHandler}>
+        <input onChange={(e)=>setEmail(e.target.value)}/>
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
   )
 }
 
