46725a67756e2c86ee73bd6c80d10bc74d2598a7
Gabriel <gabyiy2000@hotmail.com>
2022-09-15 18:20:30 +0200

    next

--- frontend/src/pages/howToMakeRecipe/HowToMakeRecipe.js
+++ frontend/src/pages/howToMakeRecipe/HowToMakeRecipe.js
@@ -3,12 +3,10 @@ import { Link, useNavigate, useParams } from "react-router-dom";
 import axios from "axios";
 import LoadingBox from "../../components/LoadingBox";
 import { Store } from "../../Store";
-import MessageBox from '../../components/messageBox/MessageBox.js';
-
+import MessageBox from "../../components/messageBox/MessageBox.js";
 
 const reducer = (state, action) => {
-
-  const navigate = useNavigate
+  const navigate = useNavigate;
 
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -23,20 +21,17 @@ const reducer = (state, action) => {
 };
 
 const HowToMakeRecipe = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { state } = useContext(Store);
   const { userInfo } = state;
 
-  const [comment,setComment]=useState("")
-const[name,setName]=useState("")
-const [refresh,setRefresh]=useState(false)
+  const [comment, setComment] = useState("");
+  const [name, setName] = useState("");
+  const [refresh, setRefresh] = useState(false);
 
-const[recipes,setRecipes]=useState([])
+  const [recipes, setRecipes] = useState([]);
   const params = useParams();
   const { _id } = params;
-
-
 
   const [{ loading, error, recipe }, dispach] = useReducer(reducer, {
     loading: true,
@@ -50,78 +45,78 @@ const[recipes,setRecipes]=useState([])
         dispach({ type: "FETCH_REQUEST" });
         const result = await axios.get(`/api/recipes/makeRecipe/${_id}`);
         dispach({ type: "FETCH_SUCCESS", payload: result.data });
-        setName(userInfo.name)
+        setName(userInfo.name);
       } catch (error) {
         dispach({ type: "FETCH_FAIL", payload: error.data });
       }
     };
     fetchMainRecipe();
-  }, [_id,error,refresh]);
+  }, [_id, error, refresh]);
 
-
-
-const submitHandler= async(e)=>{
-  e.preventDefault()
-  try{
-const{ data }=await axios.post(`/api/recipes/${_id}/reviews`,{
-comment,name
-})
-setRefresh(refresh=>!refresh)
-  }catch(err){
-
-  }
-  setComment("")
-}
-
+  console.log(recipe);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(`/api/recipes/${_id}/reviews`, {
+        comment,
+        name,
+      });
+      setRefresh((refresh) => !refresh);
+    } catch (err) {}
+    setComment("");
+  };
 
   return (
     <div>
-  {loading ? (
-    <LoadingBox></LoadingBox>
-  ) : error ? (
-    <h1>{error}</h1>
-  ) : (
-   <div>
-    <div>
-  {recipe.description}
-</div>
+      {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <h1>{error}</h1>
+      ) : (
+        <div>
+          <div>
+            <h1>{recipe.featured}</h1>
+          </div>
+          <div>{/* <p>{recipe.recipeDetails[0].firstTextFirstImg}</p> */}</div>
 
-   
-  
-  <div>
-  <h2>Reviews</h2>
-        {recipe.reviews.length === 0 && (<MessageBox>There is no messages</MessageBox>)}
-        <ul>
-          {recipe.reviews.map((review)=>(
-           <li key={review._id}>
-<strong>{review.name}</strong>
-<p>{review.createdAt.substring(0, 10)}</p>
-<p>{review.comment}</p>
-           </li> 
-          ))}
-          <li>
-            {name?
-            <form onSubmit={submitHandler}>
-<div>
-  <h2>Write a comment</h2>
-</div>
-<div>
-  <textarea value={comment} onChange={(e)=>setComment(e.target.value)}></textarea>
-  
-  <button type="submit">Post comment</button>
-  
-</div>
+          <div>
+            <h2>Reviews</h2>
+            {recipe.reviews.length === 0 && (
+              <MessageBox>There is no messages</MessageBox>
+            )}
+            <ul>
+              {recipe.reviews.map((review) => (
+                <li key={review._id}>
+                  <strong>{review.name}</strong>
+                  <p>{review.createdAt.substring(0, 10)}</p>
+                  <p>{review.comment}</p>
+                </li>
+              ))}
+              <li>
+                {name ? (
+                  <form onSubmit={submitHandler}>
+                    <div>
+                      <h2>Write a comment</h2>
+                    </div>
+                    <div>
+                      <textarea
+                        value={comment}
+                        onChange={(e) => setComment(e.target.value)}
+                      ></textarea>
 
-            </form>
-            :<div>You need to login to leave a cmment</div>}
-          </li>
-        </ul>
-  </div>
-  </div>
-  )}
-  </div>
-  )
+                      <button type="submit">Post comment</button>
+                    </div>
+                  </form>
+                ) : (
+                  <div>You need to login to leave a cmment</div>
+                )}
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default HowToMakeRecipe;
-
