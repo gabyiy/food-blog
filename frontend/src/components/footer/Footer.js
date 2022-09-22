c65bc63828975dbd442a1993d366912ba458fdca
Gabriel <gabyiy2000@hotmail.com>
2022-09-22 18:46:01 +0200

    next

--- frontend/src/components/footer/Footer.js
+++ frontend/src/components/footer/Footer.js
@@ -1,30 +1,53 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="footerMainDiv">
-      <div>Food Blog</div>
+      <div className="footerTitle">
+        <Link className="linkFooter" to="/">
+          <span>Food Blog</span>
+        </Link>
+      </div>
       <div className="detailsFooter">
         <div>Easy Recipes </div>
         <div>
-          <div>
-            <p>Faloow us</p>
+          <div className="allInconsFooter">
+            <p>FOLLOW US </p>
+            <div className="iconsFooter">
+              <div className="iconFooter">
+                <a href="https://www.facebook.com">
+                  <i class="fa fa-brands fa-facebook"></i>
+                </a>
+              </div>
+              <div className="iconFooter">
+                <a href="https://www.instagram.com">
+                  <i class=" fa fa-brands fa-instagram"></i>
+                </a>
+              </div>
+              <div className="iconFooter">
+                <a href="https://twitter.com/?lang=en">
+                  <i class="fa fa-brands fa-twitter"></i>
+                </a>
+              </div>
+            </div>
           </div>
-          <div>icons</div>
         </div>
         <div>
-          <div>Contact us</div>
-          <div>gabyiy</div>
+          <div className="contactUsFooter">CONTACT US</div>
+          <div>gabyiy2000@yahoo.com</div>
         </div>
       </div>
-      <hr></hr>
+      <hr className="hrFooter"></hr>
       <div className="termsBlog">
         <div>
-          <p>2022 Food Blog</p>
+          <p>{currentYear} Food Blog. All Rights Reserved</p>
         </div>
-        <div>
-          <p>Term of use</p>
+        <div className="privacyTerms">
+          <p>PRIVACY POLICY</p>
+          <p>TERMS AND CONDITIONS</p>
         </div>
       </div>
     </div>
