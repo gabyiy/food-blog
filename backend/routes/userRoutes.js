47b952cd7fb44f24c0f10ca5b97736ba4d7cef13
io <gabi>
2022-11-06 11:42:13 +0100

    next

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -66,9 +66,9 @@ userRouter.post(
         { expiresIn: "5m" }
       );
   //    const link = `https://food-blog-gabi.herokuapp.com/reset-password/${oldUser._id}/${token}`;
-  //const link = `https://mern-food-blog-app.onrender.com/reset-password/${oldUser._id}/${token}`;
+  const link = `https://mern-food-blog-app.onrender.com/reset-password/${oldUser._id}/${token}`;
 
- const link = `http://localhost:3000/reset-password/${oldUser._id}/${token}`;
+ //const link = `http://localhost:3000/reset-password/${oldUser._id}/${token}`;
       var transporter = nodemailer.createTransport({
         service: "gmail",
         auth: {
